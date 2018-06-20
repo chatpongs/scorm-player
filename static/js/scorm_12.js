f4f432482d11e192944a4c4659b5c365a68b2966
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-06-20 09:42:46 +0700

    add fake scorm API

---
+++ static/js/scorm_12.js
@@ -0,0 +1,54 @@
+function SCORMapi1_2() {
+    var cmi;
+    function initdatamodel(scoid) {
+        cmi = new Object();
+        cmi.core = new Object(); 
+        cmi.core.lesson_status = 'not attempted';
+    }
+
+    function LMSInitialize(param) {
+        initdatamodel(undefined); 
+        return "true";
+    }
+
+    function LMSFinish(param) {
+        return "true";
+    }
+
+    function LMSGetValue(element) {
+        var value = eval(element);
+        return value ? value : '';
+    }
+
+    function LMSSetValue(element, value) {
+        eval(element + "=" + "'" + value + "'");
+        return "true";
+    }
+
+    function LMSCommit(param) {
+        return "true";
+    }
+
+    function LMSGetLastError() {
+        return "";
+    }
+
+    function LMSGetErrorString(param) {
+        return "";
+    }
+
+    function LMSGetDiagnostic(param) {
+        return "";
+    }
+
+    this.LMSInitialize = LMSInitialize; 
+    this.LMSFinish = LMSFinish; 
+    this.LMSGetValue = LMSGetValue; 
+    this.LMSSetValue = LMSSetValue; 
+    this.LMSCommit = LMSCommit; 
+    this.LMSGetLastError = LMSGetLastError; 
+    this.LMSGetErrorString = LMSGetErrorString; 
+    this.LMSGetDiagnostic = LMSGetDiagnostic
+}
+
+window.API = new SCORMapi1_2();
